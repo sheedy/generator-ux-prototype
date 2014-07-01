9a540f1bb378229caa35dabc608d8652ffd5d685
Michael Sheedy <git@michaelsheedy.com>
2014-07-01 03:51:24 -0400

    removing tests until I know how to use them

--- test/test-load.js
+++ test/test-load.js
@@ -1,10 +1,10 @@
 /*global describe, beforeEach, it*/
-'use strict';
-var assert = require('assert');
+// 'use strict';
+// var assert = require('assert');
 
-describe('ux-prototype generator', function () {
-  it('can be imported without blowing up', function () {
-    var app = require('../app');
-    assert(app !== undefined);
-  });
-});
+// describe('ux-prototype generator', function () {
+//   it('can be imported without blowing up', function () {
+//     var app = require('../app');
+//     assert(app !== undefined);
+//   });
+// });
